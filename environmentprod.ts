f9757a305eca609f0c5ca0e0682797cbc5960321
parfait ebobisse epoune <parfaitebobisse@gmail.com>
2023-09-09 16:36:41 +0200

    correct home page

--- environmentprod.ts
+++ environmentprod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     api_url:'https://main--stalwart-jalebi-6ac4ed.netlify.app/v1',
     SOCKET_ENDPOINT:'https://main--stalwart-jalebi-6ac4ed.netlify.app',
-    keyEncryption:'parfait'
+    keyEncryption:'parfait' 
  };
