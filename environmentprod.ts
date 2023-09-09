51a6359dcfe34c6ce732039b3c2da5b173db660f
parfait ebobisse epoune <parfaitebobisse@gmail.com>
2023-09-09 22:33:21 +0200

    add env for prod backend

--- environmentprod.ts
+++ environmentprod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    api_url:'https://main--stalwart-jalebi-6ac4ed.netlify.app/v1',
-    SOCKET_ENDPOINT:'https://main--stalwart-jalebi-6ac4ed.netlify.app',
+    api_url:'https://orderland-game-balance-scale.onrender.com/v1',
+    SOCKET_ENDPOINT:'https://orderland-game-balance-scale.onrender.com',
     keyEncryption:'parfait' 
  };
